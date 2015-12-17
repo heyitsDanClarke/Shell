416e133214360446e07b0fa90bd43f03a7a60b61
Uldrendan <danclarke88@gmail.com>
2015-12-17 16:17:18 -0500

    Unpausing on main menu
    Fixed bug where game pauses on main menu

--- Assets/Scripts/Advertising.cs
+++ Assets/Scripts/Advertising.cs
@@ -5,6 +5,7 @@ using UnityEngine.Advertisements;
 public class Advertising : MonoBehaviour {
 
 	void Start () {
+		Time.timeScale = 1;
 		#if UNITY_ANDROID
 		Advertisement.Initialize ("1020810",true);
 		#elif UNITY_IPHONE
