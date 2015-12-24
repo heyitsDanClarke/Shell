bee243c27136683a90eba107f3ec0239c15fc0f5
Uldrendan <danclarke88@gmail.com>
2015-12-23 22:00:59 -0500

    Camera Script now changing appropriately for deflect
    Camera changes colour based on score in deflect mode.  Every 1000
    points colour changes to new background.  Will use Iris’ palette once I
    have access to it.

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -2,24 +2,24 @@
 using System.Collections;
 
 public class CameraScript : MonoBehaviour {
-	Color turquoise = new Color (64, 224, 208);
-	Color green = new Color (153, 201, 83);
-	Color grey = new Color (131, 144, 152);
-	Color orange = new Color (245, 109, 68);
-	Color blue = new Color (86, 132, 227);
-	Color yellow = new Color (246, 215, 81);
-	Color pink = new Color (230, 93, 113);
-	Color purple = new Color (143, 67, 201);
+	Color32 turquoise = new Color32 (64, 224, 208,255);
+	Color32 green = new Color32 (153, 201, 83,255);
+	Color32 grey = new Color32 (131, 144, 152,255);
+	Color32 orange = new Color32 (245, 109, 68,255);
+	Color32 blue = new Color32 (86, 132, 227,255);
+	Color32 yellow = new Color32 (246, 215, 81,255);
+	Color32 pink = new Color32 (230, 93, 113,255);
+	Color32 purple = new Color32 (143, 67, 201,255);
 
-	Color[] colors;
+	Color32[] colors;
 
 	int currentColor = 0;
 	int newColor;
 
-	bool colorChanged = true;
+	int currentScore = 0;
 
 	void Start(){
-		colors = new Color[8];
+		colors = new Color32[8];
 		colors [0] = turquoise;
 		colors [1] = green;
 		colors [2] = grey;
@@ -28,38 +28,26 @@ public class CameraScript : MonoBehaviour {
 		colors [5] = yellow;
 		colors [6] = pink;
 		colors [7] = purple;
-
-		Camera.main.backgroundColor = colors[2];
-		Debug.Log (Camera.main.backgroundColor);
 	}
 
 	void Update(){
-		if (GameMaster_Deflect.score % 1000 == 0 && GameMaster_Deflect.score != 0 && colorChanged) {
-			colorChanged = false;
-			newColor = Random.Range (0, 8);
+		if (GameMaster_Deflect.score % 1000 == 0 && GameMaster_Deflect.score > currentScore) {
+			currentScore = GameMaster_Deflect.score;
+			StartCoroutine (ChangeColour ());
 		}
-		if (!colorChanged) {
-			//Camera.main.backgroundColor = Color.Lerp (colors [currentColor], colors [newColor], Time.deltaTime);
-			Camera.main.backgroundColor = colors[newColor];
-			Debug.Log (Camera.main.backgroundColor);
-			if (Camera.main.backgroundColor == colors [newColor]) {
-				colorChanged = true;
-			}
-		}
-//		if (GameMaster_Deflect.score % 1000 == 0 && GameMaster_Deflect.score != 0 && colorChanged) {
-//			colorChanged = false;
-//			StartCoroutine (ChangeColour ());
-//		}
 	}
 
 	IEnumerator ChangeColour(){
-		int newColor = Random.Range (0, 8);
-		while (Camera.main.backgroundColor != colors [newColor]) {
-			Camera.main.backgroundColor = Color.Lerp (colors [currentColor], colors [newColor], Time.deltaTime);
-			Debug.Log (Camera.main.backgroundColor);
+		if (currentColor < colors.Length - 1)
+			newColor = currentColor + 1;
+		else
+			newColor = 0;
+		float t = 0;
+		while(Camera.main.backgroundColor != colors[newColor]){
+			t += Time.deltaTime;
+			Camera.main.backgroundColor = Color32.Lerp (colors[currentColor], colors[newColor], t);
 			yield return null;
 		}
 		currentColor = newColor;
-		colorChanged = true;
 	}
 }
