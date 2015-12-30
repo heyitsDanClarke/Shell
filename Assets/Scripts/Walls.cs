a91798548b15ebce727ad16f91f3cd1dc406031e
Uldrendan <danclarke88@gmail.com>
2015-12-30 01:19:47 -0500

    Changed Shield Code
    Updated shield code so that it slowly recovers over time.  Also changed
    usage from flat value to one based on time.
    
    May still need to play with these values

--- Assets/Scripts/Walls.cs
+++ Assets/Scripts/Walls.cs
@@ -11,6 +11,8 @@ public class Walls : MonoBehaviour {
 
 	bool deflect = false;
 
+	bool recovering = false;
+
 	void Start(){
 		if (SceneManager.GetActiveScene().name == "Deflect")
 			deflect = true;
@@ -25,8 +27,11 @@ public class Walls : MonoBehaviour {
 		}
 
 		if (Input.touchCount < 2) {
-			if(deflect)
+			if (deflect) {
 				transform.GetChild (2).gameObject.SetActive (false);
+				if (GameMaster_Deflect.shield < 100 && !recovering)
+					GameMaster_Deflect.shield += Time.deltaTime* 2; //adjust to change shield recovery
+			}
 			if (Input.touchCount == 1 && !EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId)) {
 				if (Input.GetTouch (0).position.x < Screen.width / 2) {
 					Rotate (Vector3.forward);
@@ -36,15 +41,23 @@ public class Walls : MonoBehaviour {
 			}
 		} else if (Input.touchCount == 2 && deflect) {
 			if (GameMaster_Deflect.shield > 0) {
-				GameMaster_Deflect.shield -= 0.5f;
+				GameMaster_Deflect.shield -= Time.deltaTime * 20; //adjust to increase rate of shield drainage
 				transform.GetChild (2).gameObject.SetActive (true);
-			}
-			else
+			} else {
 				transform.GetChild (2).gameObject.SetActive (false);
+				recovering = true;
+				StartCoroutine (ShieldRecover ());
+			}
 		}
 	}
 
     public void Rotate(Vector3 direction){
         transform.RotateAround(core.transform.position, direction, speed * Time.deltaTime);
     }
+
+	IEnumerator ShieldRecover(){
+		yield return new WaitForSeconds (5);
+		GameMaster_Deflect.shield = 100;
+		recovering = false;
+	}
 }
