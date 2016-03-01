973d8b78d75f4ff67347057d2dfa88aa878d0fd3
Uldrendan <danclarke88@gmail.com>
2016-03-01 01:02:23 -0500

    Added assets for fast and slow bullets
    Small code refactoring for the shooter as well.

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,37 +4,42 @@ using System.Collections;
 public class Shooter : MonoBehaviour {
 
 	public GameObject bullet;
+    public GameObject fastBullet;
+    public GameObject slowBullet;
 
-	bool fireReady = false;
 	float timer = 0;
 	public float fireTime =2;
 	Vector3 spawnPos;
 	
-
-	// Update is called once per frame
 	void Update () {
 		timer += Time.deltaTime;
-		if (timer >= fireTime) {
-			fireReady = true;
-			timer = 0;
-		}
-		if (fireReady) {
-			int shootWall = Random.Range(0,4);
-			if(shootWall == 0){ //left
-				spawnPos = new Vector3(0, Random.Range(0f,1f),1);
-			}
-			else if (shootWall == 1){ //top
-				spawnPos = new Vector3(Random.Range(0f,1f),1,1);
-			}
-			else if (shootWall == 2){ //right
-				spawnPos = new Vector3(1, Random.Range(0f,1f),1);
-			}
-			else { //bottom
-				spawnPos = new Vector3(Random.Range(0f,1f),0,1);
-			}
-			spawnPos = Camera.main.ViewportToWorldPoint(spawnPos);
-			Instantiate(bullet, spawnPos, transform.rotation);
-			fireReady = false;
-		}
+        if (timer >= fireTime)
+        {
+            timer = 0;
+            Fire();
+        }
 	}
+
+    void Fire()
+    {
+        int shootWall = Random.Range(0, 4);
+        if (shootWall == 0)
+        { //left
+            spawnPos = new Vector3(0, Random.Range(0f, 1f), 1);
+        }
+        else if (shootWall == 1)
+        { //top
+            spawnPos = new Vector3(Random.Range(0f, 1f), 1, 1);
+        }
+        else if (shootWall == 2)
+        { //right
+            spawnPos = new Vector3(1, Random.Range(0f, 1f), 1);
+        }
+        else
+        { //bottom
+            spawnPos = new Vector3(Random.Range(0f, 1f), 0, 1);
+        }
+        spawnPos = Camera.main.ViewportToWorldPoint(spawnPos);
+        Instantiate(bullet, spawnPos, transform.rotation);
+    }
 }
