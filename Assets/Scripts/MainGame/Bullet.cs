c60046b8b7590b425f704c7f199a826a16604df7
Daniel Clarke <danclarke88@gmail.com>
2018-03-25 00:15:28 -0400

    Implemented script that will automatically adjust bullet colour to complement the background color

---
+++ Assets/Scripts/MainGame/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    SpriteRenderer _sr;
+
+    private void Start()
+    {
+        _sr = GetComponent<SpriteRenderer>();
+        SetColourComplement();
+    }
+
+    void Update () {
+        SetColourComplement(); //remove later
+	}
+
+    void SetColourComplement()
+    {
+        float hueHolder,satHolder,value = -1;
+        Color.RGBToHSV(Camera.main.backgroundColor, out hueHolder, out satHolder, out value);
+        hueHolder *= 360;
+        hueHolder = (hueHolder + 180) % 360;
+        hueHolder /= 360;
+        _sr.color = Color.HSVToRGB(1-hueHolder, satHolder, value);
+    } 
+}
