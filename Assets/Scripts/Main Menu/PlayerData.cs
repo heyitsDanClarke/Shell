30329bd95765eddc08ae87f1107eca046768851c
Daniel Clarke <danclarke88@gmail.com>
2018-04-29 01:41:09 -0400

    Add basic purchaser script

---
+++ Assets/Scripts/Main Menu/PlayerData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    public static PlayerData Instance;
+    public int bits = 0;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else if (Instance != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+    }
+}
